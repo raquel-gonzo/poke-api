a0e0fa62886195f6833371da4070e7f0c64e5dd7
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-01 20:25:02 -0500

    boilerplate code and basic styling. first commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Click the button to diplay the names of all 807 Pokemon using the <a target='black' href='https://pokeapi.co/'>Pokemon API</a>!</h3>
+      <button>Fetch Pokemon</button>
+
+      <div id="box">
+
+      </div>
     </div>
   );
 }
